f0b7b43df97ee06d5f274bb5a5f760bee3bf81ea
surajmaheshwari123 <47104926+surajmaheshwari123@users.noreply.github.com>
2026-06-09 22:20:34 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet <Integer> set = new HashSet<Integer>();
+        for (int n : nums){
+            if(set.contains(n)) return true;
+            else set.add(n);
+        } 
+        return false;
+    }
+}
